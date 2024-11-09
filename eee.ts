9b1e76dd67685b2fc6059c2762c3ba988b28f1e7
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2024-11-09 23:19:07 +0100

    Create eee.ts

---
+++ eee.ts
@@ -0,0 +1,36 @@
+import { xxx } from './bridgeTest';
+
+
+class aaa (a , b , c ) {
+
+    let hhh ;
+    let zzz ;
+
+    function philip1 ( Q1 ) { 
+        let xxx ;
+
+        function philip2 ( Q2 ) { 
+
+            global ttt;
+            
+            let xxx ;
+        }
+ 
+        let yyy ;
+    }
+    
+}
+
+function ffffffffff ( Q3 ) { 
+    let ccc ;
+    let ddd ;
+
+    function philip1 ( Q3 ) { 
+        let ccc ;
+        let ddd ;
+    }
+}
+
+aaa.philip1;
+
+
